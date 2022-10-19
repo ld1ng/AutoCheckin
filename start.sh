bba60cd413f388b127e419818bf1999a635f233f
Ld1ng <52816450+ld1ng@users.noreply.github.com>
2022-10-19 19:40:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 echo '30 8 * * * cd /root/AutoCheckin/; /usr/bin/python dayReport.py >> log.txt 2>&1' >> /var/spool/cron/crontabs/root
-echo '59 18 * * * sleep 52; cd /root/AutoCheckin/; /usr/bin/python getLecture.py >> log2.txt 2>&1' >> /var/spool/cron/crontabs/root
+echo '59 18 * * * cd /root/AutoCheckin/; /usr/bin/python getLecture.py >> log2.txt 2>&1' >> /var/spool/cron/crontabs/root
 crontab -u root /var/spool/cron/crontabs/root
