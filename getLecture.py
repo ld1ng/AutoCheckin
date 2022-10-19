2dc90fdd362fb6facd12952f4e51fc51f4379e4e
Ld1ng <52816450+ld1ng@users.noreply.github.com>
2022-10-19 19:37:49 +0800

    Update getLecture.py
    全自动，嘎嘎乱抢

--- getLecture.py
+++ getLecture.py
@@ -12,18 +12,6 @@ with open("./config.json", "r", encoding="utf-8") as f:
     configs = f.read()
 configs = json.loads(configs)
 
-def fetch_lecture(hd_wid: str, ss, ver_code):
-    url = "http://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/hdyy/yySave.do"
-    data_json = {'HD_WID': hd_wid, 'vcode': ver_code}
-    form = {"paramJson": json.dumps(data_json)}
-    r = ss.post(url, data=form)
-    result = r.json()
-    if result['success'] is not False:
-        print(result)
-        # sys.exit(0)
-    return result['code'], result['msg'], result['success']
-
-
 def get_code(ss):
     c_url = "http://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/hdyy/vcode.do"
     c = ss.get(c_url)
@@ -32,7 +20,7 @@ def get_code(ss):
     c = ocr.classification(c_img)
     return c, c_img
 
-def get_lecture_list(ss):
+def get_leclist(ss):
     url = "http://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/*default/index.do#/hdyy"
     ss.get(url)
     # url = "http://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/modules/hdyy/hdxxxs.do"
@@ -43,59 +31,63 @@ def get_lecture_list(ss):
     rows = response['datas']
     return rows
 
-# def get_lecture_info(w_id, ss):
-#     url = "http://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/modules/hdyy/hdxxxq_cx.do"
-#     data_json = {'WID': w_id}
-#     r = ss.post(url, data=data_json)
-#     try:
-#         result = r.json()['datas']['hdxxxq_cx']['rows'][0]
-#         return result
-#     except Exception:
-#         print("信息获取失败")
-#         return False
+def catch_lec(hd_wid: str, ss, ver_code):
+    url = "http://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/hdyy/yySave.do"
+    data_json = {'HD_WID': hd_wid, 'vcode': ver_code}
+    form = {"paramJson": json.dumps(data_json)}
+    r = ss.post(url, data=form)
+    result = r.json()
+    if result['success'] is True:
+        print(result)
+        # sendmail(0,"[*] 抢课成功!","***")
+        sys.exit(0)
+    return result['code'], result['msg'], result['success']
 
 def checkdate(lecture):
-    current_time = time.strftime("%Y-%m-%d")
-    begin_time = lecture['YYKSSJ'].split(" ")[0]
-    if(current_time == begin_time):
+    currtime = time.strftime("%Y-%m-%d")
+    startime = lecture['YYKSSJ'].split(" ")[0]
+    begintime = int(time.mktime(time.strptime(lecture['YYKSSJ'], "%Y-%m-%d %H:%M:%S")))
+    if(currtime == startime):
+        while(int(time.time()) < begintime):
+            # print("请等待[" + str(begintime - int(time.time())) + "]秒，马上开始！")
+            time.sleep(1)
+        print(time.ctime(),"开始抢课！")
         return 1
     else:
-        print("[" + current_time + "] " + "最近的线上讲座在" + begin_time)
+        print("[" + currtime + "] " + "最近的线上讲座在" + startime)
         return 0
 
+def lecINFO(idx,lec):
+    print('序号：' + str(idx))
+    print("讲座wid：" + lec['WID'])
+    print("讲座名称：" + lec['JZMC'])
+    print("预约开始时间：" + lec['YYKSSJ'])
+    print("预约结束时间：" + lec['YYJSSJ'])
+    print("讲座时间：" + lec['JZSJ'])
+    print("讲座地点: " + lec['JZDD'])
+    print("================================================")
 
 def startRob(s):
-    lecture_list = get_lecture_list(s)
-    for index, lecture in enumerate(lecture_list):
-        print('序号：' + str(index))
-        print("讲座wid：" + lecture['WID'])
-        print("讲座名称：" + lecture['JZMC'])
-        print("预约开始时间：" + lecture['YYKSSJ'])
-        print("预约结束时间：" + lecture['YYJSSJ'])
-        print("讲座时间：" + lecture['JZSJ'])
-        print("讲座地点: " + lecture['JZDD'])
-        print("==========================================")
+    lecture_list = get_leclist(s)
+    for idx, lecture in enumerate(lecture_list):
+        lecINFO(idx,lecture)
         if(lecture['JZDD'] == "钉钉" or lecture['JZDD'] == "腾讯会议" or lecture['JZDD'] =="双创中心网站（https://www.seuiec.com）"):
             if(not checkdate(lecture)):
-                print("预约未开放！")
+                print("预约未开放！\n")
                 sys.exit(0)
             v_code, _ = get_code(s)
             # print(v_code)
             i = 1
-            while True:
-                if(int(lecture['YYRS'])>=int(lecture['HDZRS'])):
-                    print("人数已满！")
+            while (i<=11):
+                if(int(lecture['YYRS'])==int(lecture['HDZRS'])):
+                    print("[*] 人数已满！")
                     break
-                code, msg, success = fetch_lecture(lecture['WID'],s,v_code)
+                code, msg, success = catch_lec(lecture['WID'],s,v_code)
                 print('第{}次请求,code：{},msg：{},success:{}'.format(i, code, msg, success))
-                if success:
-                    sendmail(0,"[*] 抢课成功!","***")
-                    break
-                if msg == '验证码错误！':
-                    v_code = get_code(s)
+                if '验证码错误' in msg:
+                    v_code, _ = get_code(s)
                 time.sleep(0.5)
                 i += 1
-        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test for argparse')
@@ -116,8 +108,15 @@ if __name__ == '__main__':
         while(configs['user']['user'+ str(cnt)]):
             ss = login(configs['user']['user'+ str(cnt)]['cardnum'], configs['user']['user'+ str(cnt)]['password'])
             if ss:
-                startRob(ss)
+                try:
+                    startRob(ss)
+                except Exception as e:
+                    print(repr(e))
+                    continue
+            else:
+                continue
             cnt+=1
-    except Exception:
+    except Exception as e:
+        print(repr(e))
         print("[*] 抢讲座完成，共"+str(cnt-1)+"个用户.\n")
         exit(0)
